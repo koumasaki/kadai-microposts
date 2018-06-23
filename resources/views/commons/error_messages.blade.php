1d1afb65b506ce94b2b3116b63e9a30656c81e7b
EC2 Default User <ec2-user@ip-172-31-6-172.us-east-2.compute.internal>
2018-06-23 13:49:06 +0000

    user login

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,5 +1,5 @@
 @if (count($errors) > 0)
-    @foreach($errors->all as $error)
-        <div class="alart alart-warnig">{{ $error }}</div>
+    @foreach($errors->all() as $error)
+        <div class="alert alert-warning">{{ $error }}</div>
     @endforeach
 @endif
